6e012558e54e814dacdf02e759850eb421196adc
MON <monbondoc@gmail.com>
2025-10-29 01:49:32 +0800

    update stats code

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -25,11 +25,16 @@ const Stats = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
-              <div key={index}>
+              <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
                 <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
+                <p
+                  className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"}
+                } leading-snug text-white/80`}>
+                  {item.text}
+                </p>
               </div>
             );
           })}
